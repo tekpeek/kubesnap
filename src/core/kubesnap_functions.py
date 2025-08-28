2e9c13c8bd8f4c3618e3d459ee0f2f92da1d4749
AvinashSubhash <avinashsubhash19@gmail.com>
2025-08-28 14:42:56 +0000

    read namespaces

--- src/core/kubesnap_functions.py
+++ src/core/kubesnap_functions.py
@@ -1,2 +1,18 @@
+from kubernetes import client, config
+import json
+
+try:
+    config.load_incluster_config()
+except config.ConfigException:
+    config.load_kube_config()
+
+v1 = client.BatchV1Api()
+v1_core = client.CoreV1Api()
+v1_core_apps = client.AppsV1Api()
+namespaces_raw=v1_core.list_namespace(_preload_content=False).json()
+print(namespaces_raw.items())
+
+
+
 def create_snapshot(namespace: str):
     return
