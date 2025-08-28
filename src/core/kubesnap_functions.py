5953b90266c5863068124883e477a4761b8a6555
AvinashSubhash <avinashsubhash19@gmail.com>
2025-08-28 17:02:39 +0000

    get namespace list

--- src/core/kubesnap_functions.py
+++ src/core/kubesnap_functions.py
@@ -6,11 +6,24 @@ try:
 except config.ConfigException:
     config.load_kube_config()
 
-v1 = client.BatchV1Api()
 v1_core = client.CoreV1Api()
-v1_core_apps = client.AppsV1Api()
-namespaces_raw=v1_core.list_namespace(_preload_content=False).json()
-print(namespaces_raw.items())
+
+# Return list of namespaces
+def get_namespaces():
+    namespaces_raw=v1_core.list_namespace(_preload_content=False).json()
+    namespaces_list=[]
+    for namespace_element in namespaces_raw['items']:
+        namespaces_list.append(namespace_element['metadata']['name'])
+
+    return namespaces_list
+
+# Return list of pods
+
+# Return list of deployments
+
+# Return list of configmaps
+
+# Return list of jobs
 
 
 
