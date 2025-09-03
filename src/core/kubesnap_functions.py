e67a13d430c12677109c50aaf144041f8fa979d3
AvinashSubhash <avinashsubhash19@gmail.com>
2025-09-03 16:52:03 +0000

    store logs to tmp dir

--- src/core/kubesnap_functions.py
+++ src/core/kubesnap_functions.py
@@ -1,5 +1,6 @@
 from kubernetes import client, config
 import json
+import tempfile
 
 try:
     config.load_incluster_config()
@@ -9,6 +10,20 @@ except config.ConfigException:
 v1_core = client.CoreV1Api()
 v1_apps = client.AppsV1Api()
 v1_batch = client.BatchV1Api()
+
+def fetch_resource_list(api,namespace):
+    resource_list = api(namespace).items
+    resource_name_array = []
+    for resource in resource_list:
+        resource_name_array.append(resource.metadata.name)
+    return resource_name_array
+
+def fetch_logs(namespace,pod_list,temp_file_path):
+    for pod in pod_list:
+        logs = v1_core.read_namespaced_pod_log(pod,namespace)
+        with open(f"{temp_file_path}/{pod}.log","w") as file:
+            file.write(logs)
+            print(f"File Created {temp_file_path}/{pod}.log")
 
 # Return list of namespaces
 def get_namespaces():
@@ -21,12 +36,15 @@ def get_namespaces():
 
 # Return list of pods
 def get_pods(namespace):
+    return fetch_resource_list(v1_core.list_namespaced_pod,namespace)
     pod_list = v1_core.list_namespaced_pod(namespace).items
     pod_name_array = []
     for pod in pod_list:
         pod_name_array.append(pod.metadata.name)
     return pod_name_array
 
+#print(get_pods("default"))
+#fetch_logs("default",get_pods("default"))
 # Return list of deployments
 def get_deployments(namespace):
     deploy_list = v1_apps.list_namespaced_deployment(namespace).items
@@ -59,4 +77,12 @@ def get_cronjobs(namespace):
     return cronjob_name_array
 
 def create_snapshot(namespace: str):
+    temp_file = tempfile.TemporaryDirectory()
+    temp_file_path = temp_file.name
+    pod_list = get_pods("default")
+    fetch_logs("default",pod_list,temp_file_path)
+    print(temp_file_path)
+    temp_file.cleanup()
     return
+
+create_snapshot("default")
